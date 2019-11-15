a3263a4676bdedae8d3257850976d5ae52278dee
Thunder0-0 <45090822+Thunder0-0@users.noreply.github.com>
2019-11-15 21:51:38 +0300

    Add files via upload

---
+++ take-reverse-with stack.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+'''
+Bir yığın(stack) yapısı kullanarak verilen bir dizinin tersini alan kod yazınız.
+____
+listeyi al
+pop ederek başka bir listeye ekle 
+
+____
+
+'''
+controlPoint1 = "Y"
+list1 = []
+list2 = []
+
+while(True):
+    
+    
+    if(True):    
+        try:
+            tempNum = int(input("Number\n: "))
+            
+        except(ValueError):
+            print("You should enter a number!")
+            break
+        
+        list1.append(tempNum)
+        
+    controlPoint1 = input("Do you want continue to adding numbers? (Y/N) \n:")
+        
+    if(controlPoint1 == "Y" or controlPoint1 == "y"):
+        continue
+    
+    elif(controlPoint1 == "N" or controlPoint1 == "n"):
+        list1Copy = list1.copy()
+        
+        x = 0
+        while(x <= len(list1)):
+            list2.append(list1.pop())
+            x =+1
+            
+        print("Original list: ", list1Copy, "\n Reversed list: ", list2)
+        break
+        
+    else:
+        print("You should enter available option like Yes or No!")
+        break
