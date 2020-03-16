1eb6b671df8b7f08dc3ca3b34a35044ca510615b
Thunder0-0 <45090822+Thunder0-0@users.noreply.github.com>
2020-03-16 12:38:00 +0300

    -
    -

---
+++ pi.py
@@ -0,0 +1,21 @@
+import random
+def findp(dot_all):
+    dot_in = 0
+    control_point = 0
+    
+    while(control_point < dot_all):
+        control_point += 1
+        
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+
+        if (1 >= x**2 + y**2):
+            dot_in += 1
+
+    return float(4 * dot_in / dot_all)
+
+try:
+    print(findp(int(input("dot_all: "))))
+
+except(ValueError):
+    print('It should take int')
